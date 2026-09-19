f930b0decc423a6f2805c897c7580160602cb9f0
cedric loslier <tenslaster@gmail.com>
2026-09-19 02:21:52 -0400

    Remove legacy SDK files

--- src/highrise/tools/__init__.py
+++
@@ -1,6 +0,0 @@
-from .logger import setup_logger
-from .validator import Validator
-from .command_handler import CommandHandler
-from .loop_task import LoopTask
-from .roles import Roles
-from .utils import Utils
